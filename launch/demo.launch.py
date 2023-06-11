c07c317547af7ac03d0e86698e941462febf21ec
ZacZhangzhuo <zac.zhangzhuo@gmail.com>
2023-06-11 22:41:28 +0200

    1.0.1

--- launch/demo.launch.py
+++ launch/demo.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("abb_crb15000_5_95", package_name="abb_crb15000_5_95_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("abb_irb4600_40_255", package_name="abb_crb15000_5_95_moveit_config").to_moveit_configs()
     return generate_demo_launch(moveit_config)
--- launch/warehouse_db.launch.py
+++ launch/warehouse_db.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_warehouse_db_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("abb_crb15000_5_95", package_name="abb_crb15000_5_95_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("abb_irb4600_40_255", package_name="abb_crb15000_5_95_moveit_config").to_moveit_configs()
     return generate_warehouse_db_launch(moveit_config)
